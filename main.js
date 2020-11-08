b54814bb322a6cf0f761b864809c310a49b6a270
itmrgodsend <itmrgodsend@gmail.com>
2020-11-08 16:42:46 +0300

    add Engine ball

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
 
-// Поле, на котором всё будет происходить, — тоже как бы переменная
-let canvas = document.getElementById('game');
-// Классическая змейка — двухмерная, сделаем такую же
-let context = canvas.getContext('2d');
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+
+let dx = 2;
+let dy = -2;
+
+let ballRadius = 10;
+
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+    if(x + dx > canvas.width || x + dx < 0) {
+        dx = -dx;
+    }
+
+    if(y + dy > canvas.height || y + dy < 0) {
+        dy = -dy;
+    }
+}
+
+setInterval(draw, 10);
