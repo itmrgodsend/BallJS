def5a3927a134e5f30477d94c33234e68bbee897
itmrgodsend <itmrgodsend@gmail.com>
2020-11-08 15:44:39 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,5 @@
+
+// Поле, на котором всё будет происходить, — тоже как бы переменная
+let canvas = document.getElementById('game');
+// Классическая змейка — двухмерная, сделаем такую же
+let context = canvas.getContext('2d');
